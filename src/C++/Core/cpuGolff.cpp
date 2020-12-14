b0344905841770bb0ea8a93516b9f4b6f4b25e3c
cesmix <72821284+cesmix@users.noreply.github.com>
2020-12-14 10:45:51 -0500

    Add files via upload

---
+++ src/C++/Core/cpuGolff.cpp
@@ -0,0 +1,15 @@
+#ifndef __CPUGOLFF
+#define __CPUGOLFF
+
+#include <math.h>
+//#include <omp.h>
+
+#include "cpuArrayOperations.cpp"
+#include "cpuArrayPermute.cpp"
+#include "cpuCoordinateTransformations.cpp"
+#include "cpuSphericalHarmonics.cpp"
+
+#endif
+
+
+
