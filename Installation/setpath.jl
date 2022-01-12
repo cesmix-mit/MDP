85637bc2d3ca9371d14dad6c41c0fbd7c0cd68f4
exapde <70904303+exapde@users.noreply.github.com>
2022-01-12 00:08:29 -0500

    Update MDP

--- Installation/setpath.jl
+++ Installation/setpath.jl
@@ -9,9 +9,10 @@ push!(LOAD_PATH, MDPpath * "src/Julia/Optimization");
 # Set Julia's PATH enviroment variable so that MDP can call external programs
 ENV["PATH"] = "/usr/local/bin:/usr/bin:/opt/local/bin:/bin:/usr/sbin:/sbin";
 
-using Preprocessing, Potential, Optimization
+using Preprocessing, Potential, Optimization, Integration 
 
 # load Snap library 
 Potential.loadsnap(MDPpath)
+
 
 
